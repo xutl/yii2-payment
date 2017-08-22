11ed59e512c16556cb5499c4a2dc4ff77e535b25
XuChao <xutongle@gmail.com>
2017-08-22 16:34:29 +0800

    init

---
+++ src/Payment.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace xutl\payment;
+
+use Yii;
+use yii\base\Component;
+
+
+class Payment extends Component {
+
+}
