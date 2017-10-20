137478e7571c296ec1b8810d990efc0f41041053
Peter <xutongle@gmail.com>
2017-10-20 16:52:15 +0800

    update

---
+++ tests/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+// ensure we get report on all possible php errors
+error_reporting(-1);
+
+define('YII_ENABLE_ERROR_HANDLER', false);
+define('YII_DEBUG', true);
+$_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
+$_SERVER['SCRIPT_FILENAME'] = __FILE__;
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+Yii::setAlias('@xutl/payment', __DIR__);
+Yii::setAlias('@xutl/payment/tests', dirname(__DIR__));
+
+require_once(__DIR__ . '/compatibility.php');
