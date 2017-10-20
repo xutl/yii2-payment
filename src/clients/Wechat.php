6d4ab78277ea793e542dff1046de0c4977684b5a
Peter <xutongle@gmail.com>
2017-10-20 16:44:10 +0800

    网关

---
+++ src/clients/Wechat.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace xutl\payment;
+
+use Yii;
+use yii\base\InvalidConfigException;
+
+/**
+ * Class Wechat
+ * @package xutl\payment
+ */
+class Wechat extends BaseClient
+{
+    /**
+     * @var string 绑定支付的APPID
+     */
+    public $appId;
+
+    /**
+     * @var string 商户支付密钥
+     */
+    public $appKey;
+
+    /**
+     * @var string 商户号
+     */
+    public $mchId;
+
+    /**
+     * @var string 私钥
+     */
+    private $privateKey;
+
+    /**
+     * @var string 公钥
+     */
+    private $publicKey;
+
+    /**
+     * 初始化
+     */
+    public function init()
+    {
+        parent::init();
+        if (empty ($this->appId)) {
+            throw new InvalidConfigException ('The "appId" property must be set.');
+        }
+        if (empty ($this->appKey)) {
+            throw new InvalidConfigException ('The "appKey" property must be set.');
+        }
+        if (empty ($this->mchId)) {
+            throw new InvalidConfigException ('The "mchId" property must be set.');
+        }
+        if (empty ($this->privateKey)) {
+            throw new InvalidConfigException ('The "privateKey" property must be set.');
+        }
+        if (empty ($this->publicKey)) {
+            throw new InvalidConfigException ('The "publicKey" property must be set.');
+        }
+
+        $privateKey = "file://" . Yii::getAlias($this->privateKey);
+        $this->privateKey = openssl_pkey_get_private($privateKey);
+        if ($this->privateKey === false) {
+            throw new InvalidConfigException(openssl_error_string());
+        }
+
+        $publicKey = "file://" . Yii::getAlias($this->publicKey);
+        $this->publicKey = openssl_pkey_get_public($publicKey);
+        if ($this->publicKey === false) {
+            throw new InvalidConfigException(openssl_error_string());
+        }
+    }
+
+    /**
+     * 统一下单
+     * @param array $params
+     */
+    public function unifiedOrder($params)
+    {
+
+    }
+}
