e3dfc3f21c4a4d4f3c7dedf9c855187c073a5d38
Peter <xutongle@gmail.com>
2017-12-16 15:32:47 +0800

    update

--- src/components/Wechat.php
+++ src/components/Wechat.php
@@ -10,6 +10,8 @@ namespace xutl\payment\components;
 use Yii;
 use yii\base\InvalidConfigException;
 use xutl\payment\BaseClient;
+use yii\httpclient\Client;
+use yii\httpclient\RequestEvent;
 
 /**
  * Class Wechat
@@ -17,6 +19,8 @@ use xutl\payment\BaseClient;
  */
 class Wechat extends BaseClient
 {
+    public $baseUrl = 'https://api.mch.weixin.qq.com';
+
     /**
      * @var string 绑定支付的APPID
      */
@@ -76,5 +80,102 @@ class Wechat extends BaseClient
         if ($this->publicKey === false) {
             throw new InvalidConfigException(openssl_error_string());
         }
+        $this->requestConfig['format'] = Client::FORMAT_XML;
+        $this->responseConfig['format'] = Client::FORMAT_XML;
+        $this->on(Client::EVENT_BEFORE_SEND, [$this, 'RequestEvent']);
+    }
+
+    /**
+     * 统一下单
+     * @param array $params
+     * @return mixed
+     */
+    public function unifiedOrder($params)
+    {
+        $params = $this->buildPaymentParameter([
+            'body' => !empty($params['payId']) ? $params['payId'] : '充值',
+            'out_trade_no' => $params['outTradeNo'],
+            'total_fee' => round($params['totalFee'] * 100),
+            'fee_type' => $params['currency'],
+            'spbill_create_ip' => $params['userIp'],
+            'trade_type' => $params['tradeType'],
+        ]);
+        $params['sign'] = $this->createSignature($params);
+        /** @var \yii\httpclient\Response $response */
+        $response = $this->createRequest()
+            ->setUrl('pay/unifiedorder')
+            ->setMethod('POST')
+            ->setData($params)
+            ->send();//统一下单
+        return $response->data;
+
+    }
+
+    /**
+     * 关闭订单
+     * @param string $paymentId
+     * @return bool
+     */
+    public function closeOrder($paymentId)
+    {
+        $params = [
+            'appid' => $this->appId,
+            'mch_id' => $this->mchId,
+            'out_trade_no' => $paymentId,
+            'nonce_str' => bin2hex(openssl_random_pseudo_bytes(8)),
+        ];
+        $params['sign'] = $this->createSignature($params);
+        $response = $this->api('pay/closeorder', 'POST', $params);
+        if ($response->data['trade_state'] == 'SUCCESS') {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 签名
+     * @param array $parameters
+     * @return string
+     */
+    protected function createSignature(array $parameters)
+    {
+        foreach ($parameters as $key => $value) {
+            if (null == $value || 'null' == $value || 'sign' == $key) {
+                unset($parameters[$key]);
+            }
+        }
+        reset($parameters);
+        ksort($parameters);
+        $bizString = http_build_query($parameters);
+        $bizString .= '&sign_type=MD5&key=' . $this->appKey;
+        return strtoupper(md5(urldecode(strtolower($bizString))));
+    }
+
+    /**
+     * 编译支付参数
+     * @param array $params
+     * @return mixed
+     */
+    public function buildPaymentParameter($params = [])
+    {
+        $defaultParams = [
+            'appid' => $this->appId,
+            'mch_id' => $this->mchId,
+            'nonce_str' => $this->generateRandomString(8),
+            'notify_url' => 'http://www.openedu.tv',//$this->getNoticeUrl(),
+            'device_info' => 'WEB'
+            //'device_info' => isset($this->deviceInfoMap[$params['trade_type']]) ? $this->deviceInfoMap[$params['trade_type']] : 'WEB',
+        ];
+        return array_merge($defaultParams, $params);
+    }
+
+    /**
+     * 请求事件
+     * @param RequestEvent $event
+     * @return void
+     */
+    public function RequestEvent(RequestEvent $event)
+    {
+
     }
 }
